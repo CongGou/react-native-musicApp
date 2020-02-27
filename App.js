e0f988d59c88ea537e3cab2dc7273929f4f3212a
JsPractice <1912433492@qq.com>
2020-02-27 22:03:17 +0800

    安装引入react-redux,创建仓库

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import TabNavigator from './navigator/TabNavigator';
-
-const App = () => <TabNavigator />;
+import Home from './navigator/TabNavigator';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {reducers} from './componets/redux/reducers';
+const store = createStore(reducers);
+const App = () => (
+  <Provider store={store}>
+    <Home />
+  </Provider>
+);
 
 export default App;
